e80de2d0a0351602b221b5c25fef65f2ef606912
Ionel Cristian Mărieș <contact@ionelmc.ro>
2013-10-30 19:23:56 +0200

    Add test for failure; Refactor test code a bit.

--- tests/test_stampede.py
+++ tests/test_stampede.py
@@ -5,12 +5,24 @@ import socket
 import sys
 import time
 import unittest
-from contextlib import closing
+from contextlib import closing, contextmanager
 from process_tests import TestProcess, ProcessTestCase, setup_coverage
 
 UDS_PATH = '/tmp/stampede-tests.sock'
 TIMEOUT = int(os.getenv('REDIS_LOCK_TEST_TIMEOUT', 5))
 PY3 = sys.version_info[0] == 3
+
+@contextmanager
+def test_connection(timeout=1):
+    with closing(socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)) as sock:
+        sock.settimeout(timeout)
+        sock.connect(UDS_PATH)
+        if PY3:
+            fh = sock.makefile("rwb", buffering=0)
+        else:
+            fh = sock.makefile(bufsize=0)
+        with fh:
+            yield fh
 
 class StampedeDaemonTests(ProcessTestCase):
     def assertNotIn(self, containee, container, msg=None):
@@ -22,14 +34,7 @@ class StampedeDaemonTests(ProcessTestCase):
         with TestProcess(sys.executable, __file__, 'daemon', 'test_simple') as proc:
             with self.dump_on_error(proc.read):
                 self.wait_for_strings(proc.read, TIMEOUT, 'Queues =>')
-                with closing(socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)) as sock:
-                    sock.settimeout(1)
-                    sock.connect(UDS_PATH)
-                    if PY3:
-                        fh = sock.makefile("rwb", buffering=0)
-                    else:
-                        fh = sock.makefile(bufsize=0)
-
+                with test_connection() as fh:
                     fh.write(b"first")
                     fh.write(b"-second\n")
                     line = fh.readline()
@@ -41,24 +46,33 @@ class StampedeDaemonTests(ProcessTestCase):
                        'Queues => 0 workspaces',
                     )
 
+    def test_fail(self):
+        with TestProcess(sys.executable, __file__, 'daemon', 'test_fail') as proc:
+            with self.dump_on_error(proc.read):
+                self.wait_for_strings(proc.read, TIMEOUT, 'Queues =>')
+                with test_connection() as fh:
+                    fh.write(b"first")
+                    fh.write(b"-second\n")
+                    line = fh.readline()
+                    self.assertTrue(line.startswith(b'done (job:'), line)
+                    self.wait_for_strings(proc.read, TIMEOUT,
+                       '%s:%s' % (pwd.getpwuid(os.getuid())[0], os.getpid()),
+                       'Failed job',
+                       'Exception: FAIL',
+                       'Queues => 0 workspaces',
+                    )
+
     def test_incomplete_request(self):
         with TestProcess(sys.executable, __file__, 'daemon', 'test_simple') as proc:
             with self.dump_on_error(proc.read):
                 self.wait_for_strings(proc.read, TIMEOUT, 'Queues =>')
-                with closing(socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)) as sock:
-                    sock.settimeout(2)
-                    sock.connect(UDS_PATH)
-                    if PY3:
-                        fh = sock.makefile("rwb", buffering=0)
-                    else:
-                        fh = sock.makefile(bufsize=0)
-                    with closing(fh):
-                        fh.write(b"first")
-                        line = fh.readline()
-                        self.assertEqual(line, b'')
-                        self.wait_for_strings(proc.read, TIMEOUT,
-                           'Failed to read request from client %s:%s' % (pwd.getpwuid(os.getuid())[0], os.getpid()),
-                        )
+                with test_connection(2) as fh:
+                    fh.write(b"first")
+                    line = fh.readline()
+                    self.assertEqual(line, b'')
+                    self.wait_for_strings(proc.read, TIMEOUT,
+                       'Failed to read request from client %s:%s' % (pwd.getpwuid(os.getuid())[0], os.getpid()),
+                    )
 
     def test_queue_collapse(self):
         with TestProcess(sys.executable, __file__, 'daemon', 'test_queue_collapse') as proc:
@@ -79,7 +93,7 @@ class StampedeDaemonTests(ProcessTestCase):
                     t1 = time.time()
                     result = [fh.readline() for fh, _ in clients]
                     delta = time.time() - t1
-                    if delta > 1:
+                    if delta > TIMEOUT:
                         self.fail('Jobs took too much time (%0.2f sec)' % delta)
                     self.wait_for_strings(proc.read, TIMEOUT,
                        'test_queue_collapse OK',
@@ -95,13 +109,7 @@ class StampedeDaemonTests(ProcessTestCase):
         with TestProcess(sys.executable, __file__, 'daemon', 'test_timeout') as proc:
             with self.dump_on_error(proc.read):
                 self.wait_for_strings(proc.read, TIMEOUT, 'Queues =>')
-                with closing(socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)) as sock:
-                    sock.settimeout(3)
-                    sock.connect(UDS_PATH)
-                    if PY3:
-                        fh = sock.makefile("rwb", buffering=0)
-                    else:
-                        fh = sock.makefile(bufsize=0)
+                with test_connection() as fh:
                     fh.write(b"test_timeout\n")
                     line = fh.readline()
                     self.assertTrue(line.startswith(b'fail:14 (job:'), line)
@@ -116,24 +124,25 @@ class StampedeDaemonTests(ProcessTestCase):
         with TestProcess(sys.executable, __file__, 'daemon', 'test_simple') as proc:
             with self.dump_on_error(proc.read):
                 self.wait_for_strings(proc.read, TIMEOUT, 'Queues =>')
-                with closing(socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)) as sock:
-                    sock.settimeout(1)
-                    sock.connect(UDS_PATH)
-                    if PY3:
-                        fh = sock.makefile("rwb", buffering=0)
-                    else:
-                        fh = sock.makefile(bufsize=0)
-                    fh.write(b"first-second\n")
-                    fh.close()
-                    sock.close()
-                    time.sleep(0.2)
-                    self.assertTrue(proc.is_alive)
-                    self.wait_for_strings(proc.read, TIMEOUT,
-                       '%s:%s' % (pwd.getpwuid(os.getuid())[0], os.getpid()),
-                       'JOB first-second EXECUTED',
-                       'completed. Passing back results to',
-                       'Failed to send response to ',
-                    )
+                sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+                sock.settimeout(1)
+                sock.connect(UDS_PATH)
+                if PY3:
+                    fh = sock.makefile("rwb", buffering=0)
+                else:
+                    fh = sock.makefile(bufsize=0)
+
+                fh.write(b"first-second\n")
+                fh.close()
+                sock.close()
+                time.sleep(0.2)
+                self.assertTrue(proc.is_alive)
+                self.wait_for_strings(proc.read, TIMEOUT,
+                   '%s:%s' % (pwd.getpwuid(os.getuid())[0], os.getpid()),
+                   'JOB first-second EXECUTED',
+                   'completed. Passing back results to',
+                   'Failed to send response to ',
+                )
 
     def test_highlander(self):
         from stampede import StampedeWorker
@@ -144,6 +153,8 @@ def work_dispatch(self, workspace_name):
     test_name = sys.argv[2]
     if test_name == 'test_simple':
         logging.critical('JOB %s EXECUTED', workspace_name.decode('ascii'))
+    elif test_name == 'test_fail':
+        raise Exception('FAIL')
     elif test_name == 'test_queue_collapse':
         assert workspace_name == b'test_queue_collapse'
         time.sleep(0.35)
