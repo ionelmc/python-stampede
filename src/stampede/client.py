c3b2ac4768bb205d2a21713f95204eb88e9733e7
Ionel Cristian Mărieș <contact@ionelmc.ro>
2018-09-25 10:24:00 +0300

    So `async` is a keyword in 3.7 .......

--- src/stampede/client.py
+++ src/stampede/client.py
@@ -12,8 +12,8 @@ from .utils import IS_PY2
 logger = getLogger(__name__)
 
 
-def request(path, data, async=False):
-    logger.info("request(%r, %r, async=%s)", path, data, async)
+def request(path, data, wait=True):
+    logger.info("request(%r, %r, wait=%s)", path, data, wait)
     if b"\n" in data or b"\r" in data:
         raise RuntimeError("Request data must not have line endings!")
     try:
@@ -26,37 +26,37 @@ def request(path, data, async=False):
             else:
                 fh = sock.makefile("rwb", buffering=0)
             fh.write(b"%s\n" % data)
-            if async:
+            if not wait:
                 return
             line = fh.readline()
             if not line.startswith(b"done"):
                 raise RuntimeError("Request failed: %r. Check the logs !" % line)
-            logger.info("request(%r, %r, async=%s) - DONE.", path, data, async)
+            logger.info("request(%r, %r, wait=%s) - DONE.", path, data, wait)
             return line
     except Exception:
-        logger.exception("request(%r, %r, async=%s) - FAILED:", path, data, async)
+        logger.exception("request(%r, %r, wait=%s) - FAILED:", path, data, wait)
         raise
 
 
-def request_and_spawn(cli, path, data, async=False, timeout=1):
+def request_and_spawn(cli, path, data, wait=True, timeout=1):
     socket_path = "%s.sock" % path
     if exists(socket_path):
-        logger.debug("request_and_spawn(%r, %r, %r, async=%s) - %s already exists. Checking if lock active ...",
-                     cli, path, data, async, socket_path)
+        logger.debug("request_and_spawn(%r, %r, %r, wait=%s) - %s already exists. Checking if lock active ...",
+                     cli, path, data, wait, socket_path)
         lock = FileLock(path)
         if lock.acquire():
-            logger.debug("request_and_spawn(%r, %r, %r, async=%s) - Not locked. Spawning daemon ...",
-                         cli, path, data, async)
+            logger.debug("request_and_spawn(%r, %r, %r, wait=%s) - Not locked. Spawning daemon ...",
+                         cli, path, data, wait)
             lock.release()
             os.unlink(socket_path)
             os.spawnl(os.P_NOWAIT, *cli)
     else:
-        logger.debug("request_and_spawn(%r, %r, %r, async=%s) - %s doesn't exist. Spawning daemon ...",
-                     cli, path, data, async, socket_path)
+        logger.debug("request_and_spawn(%r, %r, %r, wait=%s) - %s doesn't exist. Spawning daemon ...",
+                     cli, path, data, wait, socket_path)
         os.spawnl(os.P_NOWAIT, *cli)
 
     t = time()
     while not exists(socket_path) and time() - t < timeout:
         sleep(0.01)
 
-    return request(path, data, async=async)
+    return request(path, data, wait=wait)
