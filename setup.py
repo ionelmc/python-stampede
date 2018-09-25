95ee60bf85ab29bb804bf7db71db3b058ee53be8
Ionel Cristian Mărieș <contact@ionelmc.ro>
2018-09-25 10:28:32 +0300

    Drop 3.4 (byte type still sucks there).

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 from __future__ import absolute_import, print_function
 
 import io
-import os
 import re
 from glob import glob
 from os.path import basename
@@ -50,7 +49,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
